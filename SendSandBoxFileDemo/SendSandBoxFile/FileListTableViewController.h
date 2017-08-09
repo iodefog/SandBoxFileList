328559cfe80deeeb61bdc066a183af5143615bd0
lihongli <honglili@sohu-inc.com>
2017-08-09 12:10:05 +0800

    更新

---
+++ SendSandBoxFileDemo/SendSandBoxFile/FileListTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  FileListTableViewController.h
+//  Utility
+//
+//  Created by LHL on 16/12/26.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+/**   遍历沙盒目录下文件，如果是非文件夹。则发送邮件
+ *    使用方法 preset or push
+ */
+
+@interface FileListTableViewController : UITableViewController
+
+
+@end
